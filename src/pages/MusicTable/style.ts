93411b45081e8276b24ff5921bb90e2438a216e7
Han-Na-05-22 <gkssktkfkd5@naver.com>
2023-03-31 15:16:59 +0900

    fix/musicTable page 반응형 적용 및 css 수정

--- src/pages/MusicTable/style.ts
+++ src/pages/MusicTable/style.ts
@@ -7,12 +7,11 @@ export const MusicTableContainer = styled.div`
   flex-direction: column;
   color: ${({ theme }) => theme.colors.BlackColor};
   height: 100%;
-  padding: 100px 0px;
+  padding: 200px 0px;
   position: relative;
-
   .my-info-submit {
     position: absolute;
-    top: 30px;
+    top: 110px;
     right: 40px;
   }
 
@@ -23,7 +22,7 @@ export const MusicTableContainer = styled.div`
     div {
       width: 100%;
       &:nth-child(2) {
-        bottom: 30px;
+        bottom: -60px;
       }
     }
 
@@ -40,6 +39,7 @@ export const MusicTableContainer = styled.div`
       tr {
         th,
         td {
+          margin: 0px 5px;
           &:first-child {
             width: 40px;
           }
@@ -61,7 +61,7 @@ export const MusicTableContainer = styled.div`
           }
 
           &:nth-child(6) {
-            width: 50px;
+            width: 100px;
           }
           &:nth-child(7) {
             width: 50px;
@@ -73,37 +73,27 @@ export const MusicTableContainer = styled.div`
             width: 100px;
           }
           &:nth-child(10) {
-            width: 100px;
-          }
-          &:nth-child(11) {
-            width: 50px;
-
-            svg {
-              cursor: not-allowed;
-            }
+            width: 150px;
           }
         }
       }
+    }
+
+    tbody {
+      min-height: 100%;
+      max-height: 100%;
     }
   }
 
   @media screen and (max-width: 1660px) {
     .tabel-container {
       & > div {
-        padding: 0pc;
+        padding: 0px;
       }
 
-      .table-header-btn {
-        display: none;
-      }
       table {
-        width: 100%;
-        height: 100%;
-
         tbody {
-          padding-bottom: 90px;
-          max-height: 100%;
-          min-height: 100%;
+          padding-bottom: 0px;
         }
       }
       .label-all {
@@ -121,25 +111,6 @@ export const MusicTableContainer = styled.div`
           th,
           td {
             font-size: 12px;
-            &:first-child {
-              width: 40px;
-            }
-
-            &:nth-child(2) {
-              width: 35px;
-            }
-
-            &:nth-child(3) {
-              width: 75px;
-            }
-
-            &:nth-child(4) {
-              width: 120px;
-            }
-
-            &:nth-child(5) {
-              width: 50px;
-            }
 
             &:nth-child(6) {
               width: 45px;
@@ -163,9 +134,144 @@ export const MusicTableContainer = styled.div`
             &:nth-child(10) {
               display: none;
             }
-            &:nth-child(11) {
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 950px) {
+    thead,
+    tbody {
+      tr {
+        height: 65px;
+        th,
+        td {
+          &:first-child {
+            div {
+              label {
+                width: 15px;
+                height: 15px;
+              }
+
+              input[type="checkbox"]:checked + .label {
+                background-image: url("/svg/min-checked.svg");
+                width: 15px;
+                height: 15px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 650px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    color: ${({ theme }) => theme.colors.BlackColor};
+    height: 100%;
+    padding: 200px 0px;
+    position: relative;
+
+    .my-info-submit {
+      position: absolute;
+      top: 135px;
+      right: 20px;
+    }
+
+    .tabel-container {
+      thead,
+      tbody {
+        tr {
+          th,
+          td {
+            font-size: 10px;
+
+            &:first-child {
+              width: 40px;
+            }
+
+            &:nth-child(2) {
+              width: 35px;
+            }
+
+            &:nth-child(3) {
+              width: 75px;
+            }
+
+            &:nth-child(4) {
+              width: 250px;
+            }
+
+            &:nth-child(5) {
+              width: 100px;
+            }
+
+            &:nth-child(6) {
+              display: none;
+              width: 100px;
+            }
+            &:nth-child(7) {
+              width: 50px;
+            }
+            &:nth-child(8) {
+              width: 50px;
+            }
+            &:nth-child(9) {
+              width: 100px;
+            }
+            &:nth-child(10) {
+              width: 150px;
+            }
+          }
+        }
+      }
+
+      div {
+        &:last-child {
+          button {
+            width: 25px;
+            height: 25px;
+            font-size: 12px;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 485px) {
+    .tabel-container {
+      thead,
+      tbody {
+        tr {
+          th,
+          td {
+            font-size: 10px;
+
+            &:nth-child(2) {
+              width: 40px;
+            }
+
+            &:nth-child(3) {
               display: none;
             }
+
+            &:nth-child(6) {
+              display: none;
+            }
+          }
+        }
+      }
+
+      div {
+        &:last-child {
+          button {
+            width: 25px;
+            height: 25px;
+            font-size: 12px;
           }
         }
       }
